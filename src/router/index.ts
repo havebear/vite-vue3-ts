1f4ab513fe91884e154b53be13d8928a80de53b9
fsworld <616749285@qq.com>
2022-08-09 15:14:11 +0800

    framework: 增加页面进度条

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,16 @@
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+//全局进度条的配置
+NProgress.configure({
+  easing: 'ease', // 动画方式
+  speed: 1000, // 递增进度条的速度
+  showSpinner: false, // 是否显示加载ico
+  trickleSpeed: 200, // 自动递增间隔
+  minimum: 0.3, // 更改启动时使用的最小百分比
+  parent: 'body', //指定进度条的父容器
+})
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -46,6 +58,14 @@ const routes: Array<RouteRecordRaw> = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async () => {
+  NProgress.start()
+})
+
+router.afterEach(() => {
+  NProgress.done()
 })
 
 export default router
